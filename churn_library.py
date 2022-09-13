4aff72962d1411e6668fac5e573823af99eecfa0
Dante G Ruiz Martínez <danteruiz@me.com>
2022-09-12 23:41:52 -0500

    Refactor One Hot Encoding, RocCurve Plots, Shap Values and Classification Report

--- churn_library.py
+++ churn_library.py
@@ -4,41 +4,31 @@ and evaluate a customer churn prediction problem
 
 Author: Dante Ruiz
 '''
-
-
-
 # import libraries
-
 import shap
 import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
-
 from sklearn.preprocessing import normalize
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-
-from sklearn.metrics import plot_roc_curve, classification_report
-
+from sklearn.metrics import RocCurveDisplay, classification_report
 from ast import main
 import os
 import logging
-
 os.environ['QT_QPA_PLATFORM']='offscreen'
 sns.set()
-
 
 logging.basicConfig(
     filename='logs/results.log',
     level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
-
 
 def import_data(pth):
     '''
@@ -170,13 +160,13 @@ def train_models(X_train, X_test, y_train, y_test):
 if __name__ == "__main__":
     
     logging.info("Executing program ...")
-    
+    # Import data
     df = import_data(r"./data/bank_data.csv")
-
-    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
-   
+    # Create dependent variable
+    df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1) 
+    # Perform EDA
     perform_eda(df)
-
+    # Variable selection
     cat_columns = [
         'Gender',
         'Education_Level',
@@ -184,7 +174,6 @@ if __name__ == "__main__":
         'Income_Category',
         'Card_Category'
     ]
-
     quant_columns = [
         'Customer_Age',
         'Dependent_count', 
@@ -201,80 +190,32 @@ if __name__ == "__main__":
         'Total_Ct_Chng_Q4_Q1', 
         'Avg_Utilization_Ratio'
     ]
-
-
-
-
+    # Create X and Y
     y = df['Churn']
-    X = pd.DataFrame()
+    X = df.loc[:, quant_columns + cat_columns]
+    # Train Test Split
+    logging.info("Splitting train-test ...") 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42)    
+    # One Hot Encoding
+    logging.info("One hot encoding categorical variables ...")
+    ohe = OneHotEncoder(handle_unknown='ignore')
+    ohe.fit(X_train.loc[:, cat_columns])
+    ohe_labels = ohe.get_feature_names_out()
+    X_train_cat_clean = pd.DataFrame(ohe.transform(X_train.loc[:, cat_columns]).toarray(), columns = ohe_labels)
+    X_test_cat_clean = pd.DataFrame(ohe.transform(X_test.loc[:, cat_columns]).toarray(), columns = ohe_labels)
+    # Cleaning train-test
+    X_train_quant = X_train.loc[:, quant_columns]
+    X_test_quant = X_test.loc[:, quant_columns]
+    X_train_clean = pd.concat([X_train_quant.reset_index(), X_train_cat_clean.reset_index()], axis = 1)
+    X_test_clean = pd.concat([X_test_quant.reset_index(), X_test_cat_clean.reset_index()], axis = 1)
 
-    # gender encoded column
-    gender_lst = []
-    gender_groups = df.groupby('Gender').mean()['Churn']
-
-    for val in df['Gender']:
-        gender_lst.append(gender_groups.loc[val])
-
-    df['Gender_Churn'] = gender_lst    
-    #education encoded column
-    edu_lst = []
-    edu_groups = df.groupby('Education_Level').mean()['Churn']
-
-    for val in df['Education_Level']:
-        edu_lst.append(edu_groups.loc[val])
-
-    df['Education_Level_Churn'] = edu_lst
-
-    #marital encoded column
-    marital_lst = []
-    marital_groups = df.groupby('Marital_Status').mean()['Churn']
-
-    for val in df['Marital_Status']:
-        marital_lst.append(marital_groups.loc[val])
-
-    df['Marital_Status_Churn'] = marital_lst
-
-    #income encoded column
-    income_lst = []
-    income_groups = df.groupby('Income_Category').mean()['Churn']
-
-    for val in df['Income_Category']:
-        income_lst.append(income_groups.loc[val])
-
-    df['Income_Category_Churn'] = income_lst
-
-    #card encoded column
-    card_lst = []
-    card_groups = df.groupby('Card_Category').mean()['Churn']
-    
-    for val in df['Card_Category']:
-        card_lst.append(card_groups.loc[val])
-
-    df['Card_Category_Churn'] = card_lst
-        
-        
-    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
-                'Total_Relationship_Count', 'Months_Inactive_12_mon',
-                'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
-                'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
-                'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
-                'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
-                'Income_Category_Churn', 'Card_Category_Churn']
-
-    X[keep_cols] = df[keep_cols]
-
-    X.head()
-
-    # This cell may take up to 15-20 minutes to run
-    # train test split 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42)
-
-    # grid search
-    rfc = RandomForestClassifier(random_state=42)
+    logging.info("Training models ...")
     # Use a different solver if the default 'lbfgs' fails to converge
     # Reference: https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
     lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
 
+    # Grid search
+    rfc = RandomForestClassifier(random_state=42)
     param_grid = { 
         'n_estimators': [200],
         #'n_estimators': [200, 500],
@@ -284,78 +225,75 @@ if __name__ == "__main__":
     }
 
     cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
-    cv_rfc.fit(X_train, y_train)
+    logging.info("Training Random Forest ...")
+    cv_rfc.fit(X_train_clean, y_train)
+    logging.info("Training Logistic Regression ...")
+    lrc.fit(X_train_clean, y_train)
 
-    lrc.fit(X_train, y_train)
+    logging.info("Predicting ...")
+    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train_clean)
+    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test_clean)
 
-    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
-    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
-
-    y_train_preds_lr = lrc.predict(X_train)
-    y_test_preds_lr = lrc.predict(X_test)
+    y_train_preds_lr = lrc.predict(X_train_clean)
+    y_test_preds_lr = lrc.predict(X_test_clean)
 
     # scores
+    logging.info("Evaluating Random Forest ...")
     print('random forest results')
     print('test results')
     print(classification_report(y_test, y_test_preds_rf))
     print('train results')
     print(classification_report(y_train, y_train_preds_rf))
-
+    logging.info("Evaluating Logistic Regression ...")
     print('logistic regression results')
     print('test results')
     print(classification_report(y_test, y_test_preds_lr))
     print('train results')
     print(classification_report(y_train, y_train_preds_lr))
-
-    lrc_plot = plot_roc_curve(lrc, X_test, y_test)
-
     # plots
+    logging.info("Generating ROC curves ...")
+    lrc_plot = RocCurveDisplay.from_estimator(lrc, X_test_clean, y_test)
     plt.figure(figsize=(15, 8))
     ax = plt.gca()
-    rfc_disp = plot_roc_curve(cv_rfc.best_estimator_, X_test, y_test, ax=ax, alpha=0.8)
+    rfc_disp = RocCurveDisplay.from_estimator(cv_rfc.best_estimator_, X_test_clean, y_test, ax=ax, alpha=0.8)
     lrc_plot.plot(ax=ax, alpha=0.8)
     plt.show()
-
-    # save best model
+    # Save best models
+    logging.info("Saving models ...")
     joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
     joblib.dump(lrc, './models/logistic_model.pkl')
-
+    # Load best models
+    logging.info("Loading models ...")
     rfc_model = joblib.load('./models/rfc_model.pkl')
     lr_model = joblib.load('./models/logistic_model.pkl')
-
-    lrc_plot = plot_roc_curve(lr_model, X_test, y_test)
-
+    # Plot from best models
+    logging.info("Generating ROC curves ...")
+    lrc_plot = RocCurveDisplay.from_estimator(lr_model, X_test_clean, y_test)
     plt.figure(figsize=(15, 8))
     ax = plt.gca()
-    rfc_disp = plot_roc_curve(rfc_model, X_test, y_test, ax=ax, alpha=0.8)
+    rfc_disp = RocCurveDisplay.from_estimator(rfc_model, X_test_clean, y_test, ax=ax, alpha=0.8)
     lrc_plot.plot(ax=ax, alpha=0.8)
     plt.show()
-
-    #explainer = shap.TreeExplainer(cv_rfc.best_estimator_)
-    #shap_values = explainer.shap_values(X_test)
-    #shap.summary_plot(shap_values, X_test, plot_type="bar")
-
+    # Shap values
+    logging.info("Generating Shap Value Plot for Random Forest ...")
+    explainer = shap.TreeExplainer(cv_rfc.best_estimator_)
+    shap_values = explainer.shap_values(X_test_clean)
+    shap.summary_plot(shap_values, X_test_clean, plot_type="bar")
     # Calculate feature importances
     importances = cv_rfc.best_estimator_.feature_importances_
     # Sort feature importances in descending order
     indices = np.argsort(importances)[::-1]
-
     # Rearrange feature names so they match the sorted feature importances
-    names = [X.columns[i] for i in indices]
-
+    names = [X_train_clean.columns[i] for i in indices]
     # Create plot
     plt.figure(figsize=(20,5))
-
     # Create plot title
     plt.title("Feature Importance")
     plt.ylabel('Importance')
-
     # Add bars
-    plt.bar(range(X.shape[1]), importances[indices])
-
+    plt.bar(range(X_train_clean.shape[1]), importances[indices])
     # Add feature names as x-axis labels
-    plt.xticks(range(X.shape[1]), names, rotation=90);
-
+    plt.xticks(range(X_train_clean.shape[1]), names, rotation=90);
     plt.rc('figure', figsize=(5, 5))
     #plt.text(0.01, 0.05, str(model.summary()), {'fontsize': 12}) old approach
     plt.text(0.01, 1.25, str('Random Forest Train'), {'fontsize': 10}, fontproperties = 'monospace')
@@ -363,12 +301,10 @@ if __name__ == "__main__":
     plt.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 10}, fontproperties = 'monospace')
     plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
     plt.axis('off');
-
     plt.rc('figure', figsize=(5, 5))
     plt.text(0.01, 1.25, str('Logistic Regression Train'), {'fontsize': 10}, fontproperties = 'monospace')
     plt.text(0.01, 0.05, str(classification_report(y_train, y_train_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
     plt.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 10}, fontproperties = 'monospace')
     plt.text(0.01, 0.7, str(classification_report(y_test, y_test_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace') # approach improved by OP -> monospace!
     plt.axis('off');
-
     logging.info("Execution SUCCESSFULL")
